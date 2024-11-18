59e31e818f8a9cf11884e27e972f42e559e954a7
marufkhan09 <maruf1609@gmail.com>
2024-11-19 01:01:06 +0600

    webview detailed implemented

--- app/src/main/java/com/app/webviewtest/MainActivity.java
+++ app/src/main/java/com/app/webviewtest/MainActivity.java
@@ -1,9 +1,15 @@
 package com.app.webviewtest;
 
 import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
@@ -27,8 +33,33 @@ public class MainActivity extends AppCompatActivity {
         // Configure WebView settings for optimal performance
         webView.getSettings().setJavaScriptEnabled(true); // Enable JavaScript if required by YouTube
         webView.getSettings().setDomStorageEnabled(true); // Enable DOM storage
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
         webView.setWebViewClient(new WebViewClient()); // Ensure links open within the WebView
-        webView.loadUrl("https://youtube.com/"); // Load the URL
+        webView.loadUrl("https://google.com/"); // Load the URL
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String s, String s1, String s2, String s3, long l) {
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(s));
+                request.setMimeType(s3);
+                String cookies = CookieManager.getInstance().getCookie(s);
+                request.addRequestHeader("cookie", cookies);
+                request.addRequestHeader("User-Agent", webView.getSettings().getUserAgentString());
+                request.setDescription("Downloading file...");
+                request.setTitle(Uri.parse(s).getLastPathSegment());
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalFilesDir(MainActivity.this, "/downloads", Uri.parse(s).getLastPathSegment());
+                DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                downloadManager.enqueue(request);
+
+                Toast.makeText(MainActivity.this, "Downloading Started", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
 
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
